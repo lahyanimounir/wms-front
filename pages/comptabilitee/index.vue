077ed37368537500868e25785391ae80cfaace54
lahyani mounir <lahyani.mo@gmail.com>
2023-02-04 11:07:21 +0100

    comptabiltée

--- pages/comptabilitee/index.vue
+++ pages/comptabilitee/index.vue
@@ -15,88 +15,115 @@
 
                 <template v-slot:top>
                     <v-toolbar flat>
-                        <v-dialog v-model="dialog" max-width="800px">
+                        <v-dialog v-model="dialog" fullscreen>
                             <template v-slot:activator="{ on, attrs }">
                                 <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
                                     Exercices
                                 </v-btn>
                             </template>
 
-                            <v-form lazy-validation ref="form" method="post" @submit.prevent="login">
-
-
-                                <v-toolbar dark color="primary">
-                                    <v-btn icon dark @click="close()">
-                                        <v-icon>mdi-close</v-icon>
-                                    </v-btn>
-                                    <v-toolbar-title>Exercice</v-toolbar-title>
-
-                                </v-toolbar>
-                                <v-card>
-
-
-                                    <v-card-text>
-                                        <v-container>
-                                            <v-row class="mb-15">
-
-
-                                                <v-col cols="6" class="py-0">
-                                                    <label for="">Du *</label>
-                                                    <div>
-                                                        <v-menu ref="menu" v-model="menu"
-                                                            :close-on-content-click="false"
-                                                            transition="scale-transition" offset-y min-width="auto">
-                                                            <template v-slot:activator="{ on, attrs }">
-                                                                <v-text-field v-model="du" outlined dense
-                                                                    prepend-icon="mdi-calendar" readonly v-bind="attrs"
-                                                                    v-on="on"></v-text-field>
-                                                            </template>
-                                                            <v-date-picker v-model="du"
-                                                            :max="au"
-                                                              ></v-date-picker>
-                                                        </v-menu>
-
-                                                    </div>
-                                                </v-col>
-                                                <v-col cols="6" class="py-0">
-                                                    <label for="">Au *</label>
-                                                    <div>
-                                                        <v-menu ref="menu" v-model="menu2"
-                                                            :close-on-content-click="false"
-                                                            transition="scale-transition" offset-y min-width="auto">
-                                                            <template v-slot:activator="{ on, attrs }">
-                                                                <v-text-field v-model="au" outlined dense
-                                                                    prepend-icon="mdi-calendar" readonly v-bind="attrs"
-                                                                    v-on="on"></v-text-field>
-                                                            </template>
-                                                            <v-date-picker v-model="au" :min="du"
-                                                              ></v-date-picker>
-                                                        </v-menu>
-
-                                                    </div>
-                                                </v-col>
 
 
 
-                                            </v-row>
-                                        </v-container>
-                                    </v-card-text>
+                            <v-toolbar dark color="primary">
+                                <v-btn icon dark @click="close()">
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                                <v-toolbar-title>Exercice</v-toolbar-title>
 
-                                    <v-card-actions>
-                                        <v-spacer></v-spacer>
-                                        <v-btn color="blue darken-1" text @click="close">
-                                            Annuler
-                                        </v-btn>
-                                        <v-btn color="blue darken-1" text @click="save">
-                                            Démarrer
-                                        </v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-form>
+                            </v-toolbar>
+                            <v-card>
+
+
+                                <v-card-text>
+                                    <v-container>
+                                        <v-card-title class="px-0 py-1"> Ajouter Exercice</v-card-title>
+
+
+                                        <v-row class="py-5 mt-2 mb-0" style="border: 1px solid #ddd;">
+
+
+
+                                            <v-col cols="6" class="py-0">
+                                                <label for="">Du *</label>
+                                                <div>
+                                                    <v-menu ref="menu" v-model="menu" :close-on-content-click="false"
+                                                        transition="scale-transition" offset-y min-width="auto">
+                                                        <template v-slot:activator="{ on, attrs }">
+                                                            <v-text-field v-model="editedItem.du" outlined dense
+                                                                prepend-icon="mdi-calendar" readonly v-bind="attrs"
+                                                                v-on="on"></v-text-field>
+                                                        </template>
+                                                        <v-date-picker v-model="editedItem.du" :max="au"></v-date-picker>
+                                                    </v-menu>
+
+                                                </div>
+                                            </v-col>
+                                            <v-col cols="6" class="py-0">
+                                                <label for="">Au *</label>
+                                                <div>
+                                                    <v-menu ref="menu" v-model="menu2" :close-on-content-click="false"
+                                                        transition="scale-transition" offset-y min-width="auto">
+                                                        <template v-slot:activator="{ on, attrs }">
+                                                            <v-text-field v-model="editedItem.au" outlined dense
+                                                                prepend-icon="mdi-calendar" readonly v-bind="attrs"
+                                                                v-on="on"></v-text-field>
+                                                        </template>
+                                                        <v-date-picker v-model="editedItem.au" :min="du"></v-date-picker>
+                                                    </v-menu>
+
+                                                </div>
+                                            </v-col>
+
+                                            <v-col lg="6" cols="12" class="py-0">
+                                                <label for="">Régime TVA</label>
+                                                <v-autocomplete :items="regimes" outlined dense placeholder="Régime TVA"
+                                                    item-text="valeur" v-model="editedItem.regimeTva" item-value="id"></v-autocomplete>
+                                            </v-col>
+
+                                            <v-col lg="6" cols="12" class="py-0">
+                                                <label for="">TVA</label>
+                                                <v-autocomplete :items="tva" outlined dense placeholder="TVA"
+                                                    item-text="valeur" v-model="editedItem.tva" item-value="id"></v-autocomplete>
+                                            </v-col>
+
+                                            <v-col lg="12" cols="12" class="py-0">
+                                                <label for="">Droit de timbre</label>
+                                                <v-autocomplete v-model="editedItem.droit_timbre" :items="timbre" outlined dense placeholder="Timbre"
+                                                    item-text="valeur" item-value="id"></v-autocomplete>
+                                            </v-col>
+                                            <v-col lg="12" cols="12" class="py-0">
+                                                <v-card-actions>
+                                                    <v-spacer></v-spacer>
+
+                                                    <v-btn color="blue darken-1" text @click="addExercice">
+                                                        Ajouter
+                                                    </v-btn>
+                                                </v-card-actions>
+                                            </v-col>
+                                        </v-row>
+                                        <v-card-title class="px-0 pb-2 pt-3"> Choisir Exercice</v-card-title>
+                                        <v-row class="py-5 ">
+                                            <v-col cols="12" class="py-0 px-0">
+                                                <v-data-table :search="search" :headers="Exerciceheaders"
+                                                    :items="dossier.exercices" style="border: 1px solid #ddd;"
+                                                    class="px-5">
+                                                </v-data-table>
+                                            </v-col>
+
+
+                                        </v-row>
+                                    </v-container>
+                                </v-card-text>
+
+
+                            </v-card>
+
                         </v-dialog>
                     </v-toolbar>
                 </template>
                 <template v-slot:item.action="{ item }">
+
                     <v-btn text color="primary" @click="startExercice(item)">
                         Démarrer l'exercice
                     </v-btn>
@@ -113,11 +140,28 @@ export default {
         dialog: false,
         dossier: {},
         rows: [],
-        menu:false,
-        menu2:false,
+        menu: false,
+        menu2: false,
+        editedItem: {
+            du: '',
+            au: '',
+            regimeTva: '',
+            tva: '',
+            droit_timbre: '',
+        },
+        defultItem: {
+            du: '',
+            au: '',
+            regimeTva: '',
+            tva: '',
+            droit_timbre: '',
+        },
         search: '',
         du: null,
         au: null,
+        regimes: [{ id: 'EXONEREE', valeur: 'Exoneré' }, { id: 'MENSUELLE', valeur: 'Mensuel' }, { id: 'TRIMESTRIELLE', valeur: 'Trimestriel' }],
+        tva: [{ id: 'DEBIT', valeur: 'Débit' }, { id: 'ENCAISSMENT', valeur: 'Encaissement' }],
+        timbre: [{ id: 'ASSUJETI', valeur: 'Assujeti' }, { id: 'NON', valeur: 'NON ASSUJETTI' }],
         headers: [
             {
                 text: 'id',
@@ -126,17 +170,24 @@ export default {
                 value: 'id',
             },
             { text: 'denomination', value: 'denomination' },
-
             { text: 'if', value: 'immf' },
             { text: 'rc', value: 'rc' },
-
             { text: 'Mobile', value: 'telephone_mobile' },
             { text: 'Email', value: 'email' },
-
-
-
-
             { text: 'Action', value: 'action' },
+        ],
+        Exerciceheaders: [
+            {
+                text: 'id',
+                align: 'start',
+                sortable: true,
+                value: 'id',
+            },
+            { text: 'Du', value: 'du' },
+            { text: 'Au', value: 'au' },
+            { text: 'Regime Tva', value: 'regimeTva' },
+            { text: 'Tva', value: 'tva' },
+            { text: 'Droit Timbre', value: 'droit_timbre' },
 
         ],
     }),
@@ -149,12 +200,18 @@ export default {
             this.rows = await this.$myService.get(url)
         },
         startExercice(item) {
-            this.$router.push('/comptabilitee/' + item.id)
-        },
-        save() {
-            
             this.dossier = item
             this.dialog = true;
+            // this.$router.push('/comptabilitee/' + item.id)
+        },
+        save() {
+
+            this.dossier = item
+            this.dialog = true;
+        },
+        async addExercice() {
+            let url = process.env.Name_api + "/dossiers";
+            this.rows = await this.$myService.post(url)
         },
         close() {
             this.dialog = false;

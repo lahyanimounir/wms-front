bd2a86f8223dbcf1e5f5b7723146c84903c2d676
ylm-py <mr.yassinelemrani@gmail.com>
2023-02-06 00:32:59 +0100

    Changed page title and added custom colors for each section

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - front-wms',
-    title: 'front-wms',
+    titleTemplate: '%s - WMS',
+    title: 'Dashboard',
     htmlAttrs: {
       lang: 'en'
     },
@@ -69,7 +69,49 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.green.accent3,
+          settings:{
+            primary: colors.grey.darken3,
+            light : colors.grey.lighten1,
+            dark : colors.grey.darken3,
+            grey: colors.grey,
+          },
+          comptabilite:{
+            primary: colors.teal.accent3,
+            light : colors.teal.lighten4,
+            dark : colors.teal.darken3,
+            teal: colors.teal,
+          },
+          paymentAndRH:{
+            primary: colors.orange.lighten2,
+            light : colors.orange.lighten4,
+            dark : colors.orange.darken3,
+            orange: colors.orange,
+          },
+          secretaireJuridique:{
+            primary:colors.indigo.darken1,
+            light : colors.indigo.lighten4,
+            dark : colors.indigo.darken3,
+            indigo: colors.indigo,
+          },
+          facturation:{
+            primary:colors.pink.lighten3,
+            light : colors.pink.lighten4,
+            dark : colors.pink.darken3,
+            pink: colors.pink,
+          },
+          documentation:{
+            primary:colors.cyan.darken1,
+            light : colors.cyan.lighten4,
+            dark : colors.cyan.darken3,
+            cyan: colors.cyan,
+          },
+          agendas:{
+            primary:colors.blueGrey.lighten4,
+            light : colors.blueGrey.lighten4,
+            dark : colors.blueGrey.darken3,
+            blueGrey: colors.blueGrey,
+          },
         }
       }
     }

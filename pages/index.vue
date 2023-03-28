6f5aa223a660559559d6ea157920434a74fbd0d4
ylm-py <mr.yassinelemrani@gmail.com>
2023-03-28 16:59:27 +0000

    My account menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,171 @@
 <template>
-  <div>
+  <v-app>
     <div class="my-account">
-      <NuxtLink to="/myAccount">
+      <template>
+        <v-container fluid>
+          <v-row justify="space-between" class="mt-1 d-flex">
+            <div class="ml-4" style="height: 50px;line-height: 50px;text-align: center;">
+              <span style="font-size: larger;margin:0">Connecté(e) en tant que : <span style="font-weight:bold">{{ user.username }}</span></span>
+            </div>
+            <v-menu  bottom min-width="200px" rounded offset-y>
+              <template  v-slot:activator="{ on }">
+                <v-btn class="mr-4" icon x-large v-on="on">
+                  <v-avatar  color="" size="48" >
+                    <img :src="user.logo" alt="User Image" class="image"/>
+                  </v-avatar>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-list-item-content class="justify-center">
+                  <div class="mx-auto text-center">
+                    <v-avatar color="">
+                      <img :src="user.logo" alt="User Image" class="image"/>
+                    </v-avatar>
+                    <h3>{{ user.username }}</h3>
+                    <p class="text-caption mt-1">
+                      {{ this.$auth.user.email }}
+                    </p>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn @click="$router.push({path:'/myAccount'})" depressed rounded text>
+                      Mon compte
+                    </v-btn>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn depressed rounded text>
+                      Deconnexion
+                    </v-btn>
+                  </div>
+                </v-list-item-content>
+              </v-card>
+            </v-menu>
+          </v-row>
+        </v-container>
+      </template>
+      <!-- <NuxtLink to="/myAccount">
         <v-btn class="btn btn-primary" style="background-color: #00897B; color: white; border-radius: 0px; border: 0px; margin-top: 20px; margin-left: 20px; margin-right: 20px; margin-bottom: 20px; font-size: 20px; font-weight: 600; height: 50px; text-transform: none; box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.2), 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 0px 0px 0px rgba(0,0,0,0.12);">
           <v-icon size="30" left>
             mdi-account-cog-outline
           </v-icon>
           Mon compte
         </v-btn>
+      </NuxtLink> -->
+      <v-menu anchor="bottom end" rounded>
+
+      </v-menu>
+    </div>
+    <div class="d-flex"
+      style="display:flex;justify-content: center;align-items: center;margin-top: 30px;flex-direction: column;">
+      <img style="" class="img-fluid" :src="iconUrl" alt="logo wms">
+      <!-- <p style="font-size:44px"><span style="color:#00897B">WE</span>B <span style="color:#00897B">MANAGE</span>MENT <span style="color:#00897B">SYSTEM</span></p> -->
+      <p style="font-size:44px">WEB MANAGEMENT SYSTEM</p>
+    </div>
+    <div class="d-flex" style="justify-content: center;align-items: center;height: 20vh">
+      <NuxtLink to="/settings/home">
+        <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+          <div>
+            <v-icon size="70" color="grey darken-3">
+
+              mdi-cog-outline
+            </v-icon>
+          </div>
+          Paramétrage
+        </v-card>
       </NuxtLink>
-    </div>
-      <div class="d-flex" style="display:flex;justify-content: center;align-items: center;margin-top: 80px;flex-direction: column;">
-        <img style="" class="img-fluid" :src="iconUrl" alt="logo wms">
-        <!-- <p style="font-size:44px"><span style="color:#00897B">WE</span>B <span style="color:#00897B">MANAGE</span>MENT <span style="color:#00897B">SYSTEM</span></p> -->
-        <p style="font-size:44px">WEB MANAGEMENT SYSTEM</p>
-      </div>
-      <v-app>
-        <div class="d-flex" style="justify-content: center;align-items: center;height: 20vh">
-          <NuxtLink to="/settings/home">
-            <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-              <div>
-                <v-icon size="70" color="grey darken-3">
-                 
-                  mdi-cog-outline
-                </v-icon>
-              </div>
-              Paramétrage
-            </v-card>
-          </NuxtLink>
-          
-          <NuxtLink to="/comptabilitee">
+
+      <NuxtLink to="/comptabilitee">
 
 
-            <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-              <div>
-                <v-icon size="70" color="teal accent-3">
-                  mdi-numeric
-                </v-icon>
-              </div>
-              Comptabilité
-            </v-card>
-          </NuxtLink>
-          <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-            <div>
-              <v-icon size="70" color="orange lighten-2">
-                mdi-account-tie
-              </v-icon>
-            </div>
-            Paie et RH
-          </v-card>
-          <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-            <div>
-              <v-icon size="70" color="indigo darken-1">
-                mdi-account-outline
-              </v-icon>
-            </div>
-            Secrétaire juridique
-          </v-card>
-          <NuxtLink to="facturation">
-            <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-              <div>
-                <v-icon size="70" color="pink lighten-3">
-                  mdi-file-document-outline 
-                </v-icon>
-              </div>
-              Facturation
-            </v-card>
-          </NuxtLink>
-          <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
-            <div>
-              <v-icon size="70" color="cyan darken-1">
-                mdi-text-box-search-outline
-              </v-icon>
-            </div>
-            Documentation
-          </v-card>
-          <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor: pointer;">
-            <div>
-              <v-icon size="70" color="blue-grey lighten-4">
-                mdi-calendar-multiple-check
-              </v-icon>
-            </div>
-            agenda et suivi des travaux
-          </v-card>
+        <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+          <div>
+            <v-icon size="70" color="teal accent-3">
+              mdi-numeric
+            </v-icon>
+          </div>
+          Comptabilité
+        </v-card>
+      </NuxtLink>
+      <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+        <div>
+          <v-icon size="70" color="orange lighten-2">
+            mdi-account-tie
+          </v-icon>
         </div>
-      </v-app>
-  
-  
-  
+        Paie et RH
+      </v-card>
+      <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+        <div>
+          <v-icon size="70" color="indigo darken-1">
+            mdi-account-outline
+          </v-icon>
+        </div>
+        Secrétaire juridique
+      </v-card>
+      <NuxtLink to="facturation">
+        <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+          <div>
+            <v-icon size="70" color="pink lighten-3">
+              mdi-file-document-outline
+            </v-icon>
+          </div>
+          Facturation
+        </v-card>
+      </NuxtLink>
+      <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor:pointer">
+        <div>
+          <v-icon size="70" color="cyan darken-1">
+            mdi-text-box-search-outline
+          </v-icon>
+        </div>
+        Documentation
+      </v-card>
+      <v-card class="px-3 py-3 ml-4 text-center cardHover" elevation="0" style="border: 1px solid #ddd;cursor: pointer;">
+        <div>
+          <v-icon size="70" color="blue-grey lighten-4">
+            mdi-calendar-multiple-check
+          </v-icon>
+        </div>
+        agenda et suivi des travaux
+      </v-card>
     </div>
-  </template>
+  </v-app>
+</template>
   
   
-  <style scoped>
-    .cardHover:hover{
-      background: #f1f1f1;
-    }
-    a{
-        text-decoration: none
-    }
-    .cardHover div{
-        padding-bottom: 5px ;
-    }
-  </style>
+<style scoped>
+.cardHover:hover {
+  background: #f1f1f1;
+}
 
-  <script>
-  export default{
-    middleware: ['auth'],
-    data: () => ({
-      iconUrl: require('../assets/images/LogoWMS.png'),
-    }),
+a {
+  text-decoration: none
+}
+
+.cardHover div {
+  padding-bottom: 5px;
+}
+
+.centered {
+  display: flex;
+  align-items: center;
+  /* height: 100px; */
+  /* Set a height to see the effect */
+}
+</style>
+
+<script>
+export default {
+  middleware: ['auth'],
+  data: () => ({
+    iconUrl: require('../assets/images/LogoWMS.png'),
+    user: {
+      initials: 'JD',
+      fullName: 'John Doe',
+      email: 'john.doe@doe.com',
+      logo:"null",
+    },
+  }),
+  created(){
+    this.user = this.$auth.user
   }
+}
 </script>
   
--- pages/myAccount/index.vue
+++ pages/myAccount/index.vue
@@ -9,13 +9,50 @@
                 </div>
                 <v-card-title>Mon compte</v-card-title>
                 <v-card-text>
-                    <v-avatar size="150">
-                        <img :src="tempImg" alt="User Image" />
-                    </v-avatar>
+                    <div>
+                        <v-avatar size="150" outlined>
+                            <img :src="preview" alt="User Image" class="image"/>
+                            <div class="overlay" @click="openFileInput">
+                                <v-icon size="38px" class="text">mdi-camera</v-icon>
+                            </div>
+                        </v-avatar>
+                        <v-file-input style="display:none" ref="fileInput" dense outlined
+                      label="Logo " @change="changeImage" hidden></v-file-input>
+                    </div>
                     <v-form ref="form">
-                        <v-text-field v-model="editedItem.username" :rules="obligationRule" label="Username"></v-text-field>
-                        <v-text-field v-model="editedItem.email" :rules="obligationRule" label="Email" type="email"></v-text-field>
-                        <v-text-field v-model="editedItem.password" :rules="obligationRule" label="Password" type="password"></v-text-field>
+                        <v-row class="my-0">
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.username" :rules="obligationRule" label="Username"></v-text-field>
+                            </v-col >
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.email" :rules="obligationRule" label="Email" type="email"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.denomination" :rules="obligationRule" label="Denomination" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.identifiant_fiscale" :rules="obligationRule" label="IF" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.ice" :rules="obligationRule" label="ICE" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.taxe_professionnel" :rules="obligationRule" label="TAXE PROFESSIONNELLE" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.adresse" :rules="obligationRule" label="ADRESSE" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.activitee" :rules="obligationRule" label="ACTIVITEE" type="text"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.telephone" :rules="obligationRule" label="TELEPHONE" type="text"></v-text-field>
+                            </v-col>
+                            
+                            <!-- <v-col cols="12" lg="6">
+                                <v-text-field v-model="editedItem.password" :rules="obligationRule" label="Password" type="password"></v-text-field>
+                            </v-col> -->
+                        </v-row>
                     </v-form>
                 </v-card-text>
                 <v-card-actions>
@@ -26,32 +63,115 @@
         </v-container>
     </v-app>
 </template>
-  
+<style>
+.v-avatar img {
+    opacity: 1;
+    display: block;
+    width: 100%;
+    height: auto;
+    transition: .5s ease;
+    backface-visibility: hidden;
+    cursor: pointer;
+}
+
+.v-avatar .overlay {
+    cursor: pointer;
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.v-avatar:hover .overlay {
+    opacity: 1;
+}
+
+.v-avatar:hover img {
+    opacity: 0.5;
+}
+
+.v-avatar .text {
+    color: rgb(0, 0, 0);
+    font-size: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+
+</style>
 <script>
 export default {
     middleware: ["auth"],
     data: () => ({
         obligationRule: [(v) => !!v || "Ce champ est obligatoire"],
-        tempImg: "https://picsum.photos/200",
+        preview: "https://pic.onlinewebfonts.com/svg/img_264157.png",
         editedItem:{
             username: "",
             email: "",
             password: "",
+            denomination: "",
+            identifiant_fiscale: "",
+            ice: "",
+            taxe_professionnel: "",
+            adresse: "",
+            activitee: "",
+            telephone: "",
+            logo: "",
         }
     }),
     async created(){
         this.editedItem = JSON.parse(JSON.stringify(this.$auth.user))
+        if (this.editedItem.logo) this.preview = this.editedItem.logo   
 
     },
     methods: {
         async saveChanges() {
             if (this.$refs.form.validate()) {
+                var formData = new FormData();
+                Object.keys(this.editedItem).forEach(key => formData.append(key, this.editedItem[key]));
+                this.editedItem.logo = this.preview
                 let url = process.env.Name_api + "/profile";
-                const res = await this.$myService.put(url, this.editedItem);
-                this.$auth.strategy.token.set(res.data.token);
-                this.$auth.setUser(res.data.user);
+                const res = await this.$myService.put(url, this.editedItem,true);
+                console.log(res)
+
+                // this.$auth.strategy.token.set(res.data.token);
+                // this.$auth.setUser(res.data.user);
             }
         },
+        async changeImage($event) {
+            const file = $event
+            if (!file) return;
+            console.log('file', file)
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                this.preview = e.target.result;
+            };
+            reader.readAsDataURL(file);
+            // store image file to send to server
+            this.editedItem.logo = file;
+
+
+
+        },
+        openFileInput() {
+            this.$nextTick(() => {
+                try {
+                console.log('Trying to trigger file input...');
+                this.$refs.fileInput.$refs.input.click();
+                } catch (error) {
+                console.error('Error triggering file input:', error);
+                }
+            });
+            },
     },
 };
 </script>
